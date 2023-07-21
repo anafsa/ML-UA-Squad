fca7b355befa644acf3fab4fbf8482b6a1b7ab81
João Alves <jbga@ua.pt>
2023-07-21 19:21:16 +0100

    Add image filtering and input arguments

--- api/image/image_generation_from_story.py
+++ api/image/image_generation_from_story.py
@@ -7,41 +7,90 @@ import re
 import pickle
 import glob
 from pyiqa import create_metric
+import argparse
+import numpy as np
 
 def brisk(image):
-
     metric_name = "brisque"
-
     iqa_model = create_metric(metric_name, metric_mode="NR")
-
     score = iqa_model(image)
-    
     return score.item()
 
+def brisk_for_multiple_images(images):
+    metric_name = "brisque"
+    iqa_model = create_metric(metric_name, metric_mode="NR")
+    scores = []
+    for image in images:
+        score = iqa_model(image)
+        scores.append(score.item())
+    return scores
 
-story_folder_path = "Default"
-for file_path in glob.glob(r"stories/*.pkl"):
-    story_folder_path = re.split(r'[\/\.]', file_path)[1]
-    with open(file_path, 'rb') as f:
+
+def multiple_images_evaluation(images,metric_name="clipiqa+"):
+    iqa_model = create_metric(metric_name, metric_mode="NR")
+    scores = []
+    for image in images:
+        score = iqa_model(image)
+        scores.append(score.item())
+    return scores
+    
+
+
+default_story_path = "stories/1689861518_Safe_and_Playful:_Timmy's_Allergy_Adventure.pkl"
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input_story', type=str, default=default_story_path, help='input image/folder path.')
+parser.add_argument('-b', '--batch_size', type=int, default=1, help='number of images to be generated per paragph.')
+parser.add_argument('-t', '--threshold', type=float, default=0.2, help='Threshold for image quality generation.')
+args = parser.parse_args()
+
+file_path = args.input_story
+if file_path == default_story_path:
+    print("Choosen story is the default story")
+    print("Make sure that you specified the story")
+
+
+if file_path.startswith("stories") == False:
+    file_path = f"stories/{file_path}"
+
+if file_path.endswith(".pkl") == False:
+    file_path = f"{file_path}.pkl"
+
+with open(file_path, 'rb') as f:
         data = pickle.load(f)
 
+story_folder_path= re.split(r'[\/\.]', file_path)[1]
+
+# story_folder_path = "Default"
+# for file_path in glob.glob(r"stories/*.pkl"):
+#     story_folder_path = re.split(r'[\/\.]', file_path)[1]
+#     with open(file_path, 'rb') as f:
+#         data = pickle.load(f)
+
 print("Story folder: ",story_folder_path)
-# print(data.keys())
-# print(data["story"])
 
 story_prompts = []
-story_title = data["cover_image_prompt"]
-story_prompts.append(story_title)
+story_title_prompt = data["cover_image_prompt"]
+divided_story_title_prompt = story_title_prompt.split(":")
+if len(divided_story_title_prompt) > 1:
+    story_title_prompt = divided_story_title_prompt[1]
+    
+story_prompts.append(story_title_prompt)
 
 story_prompts_polarity= ["neutral"]
-for story_paragraph in data["story"]:
-    story_prompts.append(story_paragraph["image_prompt"])
-    story_prompts_polarity.append(story_paragraph["polarity"])
+for i,story_paragraph_data in enumerate(data["story"]):
+    # paragh_story = story_paragraph_data["text"]
+    # print(f"Paragh {i+1}: {paragh_story}")
+    divided_image_prompt = story_paragraph_data["image_prompt"].split(":")
+    if len(divided_image_prompt) > 1:
+        story_prompts.append(divided_image_prompt[1])
+    else: 
+        story_prompts.append(story_paragraph_data["image_prompt"])
+    story_prompts_polarity.append(story_paragraph_data["polarity"])
  
     
-print(f"Story Title: {story_title}")
+print(f"Cover Image prompt: {story_title_prompt}")
 
-print(story_prompts)
+print(f"Story_prompts: {story_prompts}")
 
 
 DEVICE_CPU = torch.device('cpu:0')
@@ -83,9 +132,8 @@ decoder = KandinskyV22Pipeline.from_pretrained(
 ).to(DEVICE_GPU_0)
 
 
-num_batches = 1
-images_per_batch = 1
-total_num_images = images_per_batch * num_batches
+images_per_batch = args.batch_size
+
 
 negative_prior_prompt = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, \
     duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, \
@@ -97,36 +145,54 @@ prompt_text = f"{story_prompts[current_prompt_index]} Child Storybook illustrati
 images = []
 
 os.makedirs(story_folder_path, exist_ok = True)
+# extended_story_folder_path = story_folder_path
+extended_story_folder_path = f"{story_folder_path}/all"
+if images_per_batch > 1:
+    os.makedirs(extended_story_folder_path, exist_ok = True)
 
-print(f"*** Generating {total_num_images} image(s) ***")
 for i in range(len(story_prompts)):
     print(f"* Batch {i} of {len(story_prompts)- 1} *")
 
     print(f"Prompt: \n{prompt_text}" )
 
-    num_inference_steps = 50 
+    num_inference_steps = 50
+    bad_image_generated = True 
     
-    # Generating embeddings on the CPU
-    img_emb = prior(
-        prompt=prompt_text,
-        num_inference_steps=num_inference_steps,
-        num_images_per_prompt=images_per_batch)
+    while bad_image_generated:
+        # Generating embeddings on the CPU
+        img_emb = prior(
+            prompt=prompt_text,
+            num_inference_steps=num_inference_steps,
+            num_images_per_prompt=images_per_batch)
 
-    negative_emb = prior(
-        prompt=negative_prior_prompt,
-        num_inference_steps=num_inference_steps,
-        num_images_per_prompt=images_per_batch
-    )
+        negative_emb = prior(
+            prompt=negative_prior_prompt,
+            num_inference_steps=num_inference_steps,
+            num_images_per_prompt=images_per_batch
+        )
 
-    # Converting fp32 to fp16, to run decoder on the GPU
-    image_batch = decoder(
-        image_embeds=img_emb.image_embeds,
-        negative_image_embeds=negative_emb.image_embeds,
-        num_inference_steps=num_inference_steps, height=512, width=512)
-    
-    for i,image in enumerate(image_batch.images):
-        image_batch.images[i].save(f"{story_folder_path}/img_{current_prompt_index}_{num_inference_steps}_{i}.png")
-        print(f"Image quality: {brisk(image)}")
+        # Converting fp32 to fp16, to run decoder on the GPU
+        image_batch = decoder(
+            image_embeds=img_emb.image_embeds,
+            negative_image_embeds=negative_emb.image_embeds,
+            num_inference_steps=num_inference_steps, height=512, width=512)
+        
+        maniqa_scores = multiple_images_evaluation(image_batch.images,metric_name="maniqa")
+        clip_scores = multiple_images_evaluation(image_batch.images,metric_name="clipiqa+")
+        assembled_scores = list(np.multiply(maniqa_scores,clip_scores))
+        max_image_score = max(assembled_scores)
+        max_value_image = image_batch.images[assembled_scores.index(max_image_score)]
+        if max_image_score > args.threshold:
+            bad_image_generated = False
+        else:
+            print("Retrying image generation")
+            print(f"Max assembled score: {max_image_score:.2f}")
+
+        for i,image in enumerate(image_batch.images):
+            brisk_score = brisk(image)
+            image_batch.images[i].save(f"{extended_story_folder_path}/img_{current_prompt_index}_{clip_scores[i]:.2f}_{maniqa_scores[i]:.2f}_{assembled_scores[i]:.2f}_{i}.png")
+
+    max_value_image.save(f"{story_folder_path}/img_{current_prompt_index}_{max_image_score:.2f}.png")
     current_prompt_index +=1
     
     if current_prompt_index == len(story_prompts):
@@ -141,5 +207,3 @@ for i in range(len(story_prompts)):
         prompt_text += f", {story_prompts_polarity[current_prompt_index]}"
     
     images += image_batch.images
-
-# Possible idea: aggregate polarity
